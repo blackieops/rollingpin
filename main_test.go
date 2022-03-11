5e1dfd65f551370a5e0cace9045353a188d9d8ce
Alex Blackie <alex@blackie.me>
2022-03-10 18:44:42 -0800

    Flesh out integration test to cover kubernetes
    This is a really unbelievably unruly test now, but it is effective in
    that it covers basically as end-to-end as we can get without a full
    kubernetes cluster to test against.

--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -10,6 +11,9 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.b8s.dev/rollingpin/config"
 	"go.uber.org/zap"
+	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
 )
 
@@ -80,9 +84,7 @@ func TestAuthMismatch(t *testing.T) {
 }
 
 func TestHarborWebhookPushArtifact(t *testing.T) {
-	conf := &config.Config{AuthToken: "abc1234"}
-	api := fake.NewSimpleClientset()
-	log, _ := zap.NewProduction()
+	// Set up test HTTP request
 	payload := `{
 		"type": "PUSH_ARTIFACT",
 		"occur_at": 1586922308,
@@ -91,13 +93,13 @@ func TestHarborWebhookPushArtifact(t *testing.T) {
 			"resources": [{
 				"digest": "sha256:8a9e9863dbb6e10edb5adfe917c00da84e1700fa76e7ed02476aa6e6fb8ee0d8",
 				"tag": "latest",
-				"resource_url": "hub.harbor.com/test-webhook/debian:latest"
+				"resource_url": "cr.b8s.dev/library/debian:v2"
 			}],
 			"repository": {
 				"date_created": 1586922308,
 				"name": "debian",
-				"namespace": "test-webhook",
-				"repo_full_name": "test-webhook/debian",
+				"namespace": "library",
+				"repo_full_name": "library/debian",
 				"repo_type": "private"
 			}
 		}
@@ -105,13 +107,56 @@ func TestHarborWebhookPushArtifact(t *testing.T) {
 	req, _ := http.NewRequest("POST", "/webhooks/harbor", bytes.NewBufferString(payload))
 	req.Header.Add("authorization", "Bearer abc1234")
 	resp := httptest.NewRecorder()
+
+	// Set up fake kubernetes api client
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	api := fake.NewSimpleClientset()
+
+	// Set up initial kubernetes deployment
+	initialDeploy := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{Name: "test-deployment"},
+		Spec: appsv1.DeploymentSpec{
+			Template: v1.PodTemplateSpec{
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{
+						{Name: "app", Image: "cr.b8s.dev/library/debian:v1"},
+					},
+				},
+			},
+		},
+	}
+	api.AppsV1().Deployments("default").Create(ctx, initialDeploy, metav1.CreateOptions{})
+
+	// Set up our own app's stuff finally
+	conf := &config.Config{
+		AuthToken: "abc1234",
+		Mappings: []config.ImageMapping{
+			{
+				Namespace:      "default",
+				DeploymentName: "test-deployment",
+				ImageName:      "library/debian",
+			},
+		},
+	}
+	log, _ := zap.NewProduction()
+
+	// Execute request
 	r := buildRouter(conf, log, api)
 	r.ServeHTTP(resp, req)
+
+	// Assertions
 	if resp.Code != 200 {
 		t.Errorf("Expected 200 response got: %d", resp.Code)
 	}
 	if resp.Body.String() != `{"ok":true}` {
 		t.Errorf("Expected OK response got: %s", resp.Body.String())
+	}
+
+	newDeploy, _ := api.AppsV1().Deployments("default").Get(ctx, "test-deployment", metav1.GetOptions{})
+	newImageName := newDeploy.Spec.Template.Spec.Containers[0].Image
+	if newImageName != "cr.b8s.dev/library/debian:v2" {
+		t.Errorf("Expected deployment to be updated but was not! Image was: %s", newImageName)
 	}
 }
 
